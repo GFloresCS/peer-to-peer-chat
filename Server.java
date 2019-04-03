b46a838014987f819b150186ca781041d4b21a37
SoSickOfPlaying <43160933+SoSickOfPlaying@users.noreply.github.com>
2019-04-03 16:59:40 -0700

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,49 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread
+{
+	final ServerSocket serverS;
+	private ConnectionLists list;
+	private ConnectionLists connections;
+	
+	public Server(ServerSocket serverS, ConnectionLists list, ConnectionLists connections)
+	{
+		this.serverS = serverS;
+		this.list = list;
+		this.connections = connections;
+		System.out.println("Starting a new server!");
+	}
+	
+	@Override
+	public void run()
+	{
+		//Socket stays open to retrieve requests
+		while(true)
+		{
+			try
+			{
+				//Socket object to receive incoming client requests
+				Socket sock = serverS.accept();
+
+				System.out.println("A new client is connected.");
+
+				//Obtaining input and output streams
+				DataInputStream dis = new DataInputStream(sock.getInputStream());
+				DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+
+				//Create a new Handler
+				Handler handle = new Handler(sock, dis, dos, list);
+				handle.start();
+				connections.addList(handle);
+			}
+			catch(Exception e)
+			{
+				System.out.println("Server closed.");
+				break;
+			}
+		}
+	}
+}
